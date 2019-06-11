fd9eeb31c27bfc36c5252d70a441b11ee0af3b86
VaderSteve76 <alverson.s@yahoo.com>
2019-06-11 17:25:48 -0400

    Move to Front & End

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -91,13 +91,28 @@ class DoublyLinkedList:
             self.length += 1
 
     def remove_from_tail(self):
-        pass
+        removed = self.tail.value
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+            self.length = 0
+        else:
+            self.tail = self.head.prev
+            self.tail.next.delete()
+            self.length -= 1
+        return removed
 
     def move_to_front(self, node):
-        pass
+        self.add_to_head(node.value)
+        self.length -= 1
 
     def move_to_end(self, node):
-        pass
+        if self.length > 1 and node != self.tail:
+            self.tail.insert_after(node.value)
+            self.tail = self.tail.next
+        if node == self.head:
+            self.head = node.next
+        node.delete()
 
     def delete(self, node):
         pass
